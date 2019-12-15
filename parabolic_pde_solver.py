b258aab805d5dc34cd43e1b9b3e516eef0bf50eb
Harry Blakiston Houston <harry@phoneboxapp.co.uk>
2019-12-15 18:03:52 +0000

    Untested but have now implemented FE and BE

--- parabolic_pde_solver.py
+++ parabolic_pde_solver.py
@@ -89,7 +89,8 @@ def plot_x_t_u(x, t, Z):
     plt.show()
 
 # internal function so no description added
-def create_A_CN(x, f_kappa, deltat, deltax):
+# TODO test this
+def create_A_CN(x, f_kappa, deltat, deltax, logger=False):
     diagonals = [[],[],[]]
     for i in x:
         kappa_v = f_kappa(i)
@@ -104,10 +105,17 @@ def create_A_CN(x, f_kappa, deltat, deltax):
     diagonals[2].pop()
     # create matrix 1 and matrix 2
     ACN = csr_matrix(diags(diagonals, [-1, 0, 1]).toarray())
+    #print diagonal matrices
+    if logger:
+         print("ACN to solve for each step: \n{}".format(ACN))
+    # print values we are using
+    if logger:
+        print("deltax=",deltax); print("deltat=",deltat); print("lambda=",lmbda)
     return ACN
 
 #internal function so no description added
-def create_B_CN(x, f_kappa, deltat, deltax):
+# TODO test this
+def create_B_CN(x, f_kappa, deltat, deltax, logger=False):
     diagonals = [[],[],[]]
     for i in x:
         kappa_v = f_kappa(i)
@@ -122,19 +130,153 @@ def create_B_CN(x, f_kappa, deltat, deltax):
     diagonals[2].pop()
     # create matrix 1 and matrix 2
     BCN = csr_matrix(diags(diagonals, [-1, 0, 1]).toarray())
+    #print diagonal matrices
+    if logger:
+         print("BCN to solve for each step: \n{}".format(BCN))
+    # print values we are using
+    if logger:
+        print("deltax=",deltax); print("deltat=",deltat); print("lambda=",lmbda)
     return BCN
+
+#internal function so no description added
+# TODO test this
+def create_A_BE(x, f_kappa, deltat, deltax, logger=False):
+    diagonals =[[],[],[]]
+    for i in x:
+         kappa_v = f_kappa(i)
+         # calculate lmbda
+         lmbda = kappa_v*deltat/(deltax**2) # mesh fourier number
+         # calc -1 0 1 and add to diagonals
+         diagonals[0].append(-lmbda)
+         diagonals[1].append(1+2*lmbda)
+         diagonals[2].append(-lmbda)
+    # remove 1 value from 1st and third array
+    diagonals[0].pop()
+    diagonals[2].pop()
+    # create matrix 1 and matrix 2
+    ABE = csr_matrix(diags(diagonals, [-1, 0, 1]).toarray())
+    #print diagonal matrices
+    if logger:
+         print("ABE to solve for each step: \n{}".format(ABE))
+    # print values we are using
+    if logger:
+        print("deltax=",deltax); print("deltat=",deltat); print("lambda=",lmbda)
+    return ABE
+
+#internal function so no description added
+# TODO test this
+def create_A_FE(x, f_kappa, deltat, deltax, logger=False):
+    diagonals =[[],[],[]]
+    lmbda_vec = []
+    for i in x:
+         kappa_v = f_kappa(i)
+         # calculate lmbda
+         lmbda = kappa_v*deltat/(deltax**2) # mesh fourier number
+         lmbda_vec.append(lmbda)
+         # calc -1 0 1 and add to diagonals
+         diagonals[0].append(lmbda)
+         diagonals[1].append(1-2*lmbda)
+         diagonals[2].append(lmbda)
+    # remove 1 value from 1st and third array
+    diagonals[0].pop()
+    diagonals[2].pop()
+    # create matrix 1 and matrix 2
+    AFE = csr_matrix(diags(diagonals, [-1, 0, 1]).toarray())
+    #print diagonal matrices
+    if logger:
+         print("AFE to solve for each step: \n{}".format(AFE))
+    # print values we are using
+    if logger:
+        print("deltax=",deltax); print("deltat=",deltat); print("lambda=",lmbda)
+    return [AFE, lmbda_vec]
+
+# TODO test this
+# Solve CN
+def solve_CN(u_j, A_CN, B_CN, heat_j):
+    """
+    Returns the solution to a Crank Nicholson iteration
+
+    USAGE: solve_CN(u_j, A_CN, B_CN, heat_j)
+
+    INPUTS:
+        u_j: The current heat distribution over L
+        A_CN: The matrix used to calculate the next iteration A_CN*u_jp1=B_CN*u_j
+        B_CN: The matrix used to calculate the next iteration A_CN*u_jp1=B_CN*u_j
+
+        **optional** (default)
+        heat_j: ([0]) Any internal heat source
+
+    OUTPUT: Array of scalars len(u_j) repesenting u_jp1
+
+    NOTES:
+    """
+    # dependent var to solve
+    b = B_CN.dot(u_j)
+    # Backward Euler timestep solve matrix equation
+    u_jp1 = spsolve(A_CN, b) + heat_j
+    return u_jp1
+
+# TODO test this
+def solve_FE(u_j, A_FE, heat_j, lmbda, bc1, bc2):
+    """
+    Returns the solution to a Crank Nicholson iteration
+
+    USAGE: solve_FE(u_j, A_FE, heat_j)
+
+    INPUTS:
+        u_j: The current heat distribution over L
+        A_FE: The matrix used to calculate the next iteration u_jp1=A_FE*u_j
+
+        **optional** (default)
+        heat_j: ([0]) Any internal heat source
+
+    OUTPUT: Array of scalars len(u_j) repesenting u_jp1
+
+    NOTES:
+    """
+
+    rhs_vector = np.zeros(len(u_j))
+    rhs_vector[0] = bc1
+    rhs_vector[-1] = bc2
+    u_jp1 = A_FE.dot(u_j) + heat_j + np.multiply(lmbda,rhs_vector)
+    return u_jp1
+
+
+# TODO test this
+def solve_BE(u_j, A_BE, heat_j):
+    """
+    Returns the solution to a Crank Nicholson iteration
+
+    USAGE: solve_BE(u_j, A_FE, heat_j)
+
+    INPUTS:
+        u_j: The current heat distribution over L
+        A_BE: The matrix used to calculate the next iteration A_BE*u_jp1=u_j
+
+        **optional** (default)
+        heat_j: ([0]) Any internal heat source
+
+    OUTPUT: Array of scalars len(u_j) repesenting u_jp1
+
+    NOTES:
+    """
+    u_jp1 = spsolve(A_BE,u_j) + heat_j
+    return u_jp1
+
 
 #set up the function with args
 def pde_solve(L, T, u_I, mx, mt, f_kappa=lambda x: 1,
         logger=True, bcf=lambda t: [0,0], plot=False,
-        heat_source=lambda x, t: [0], plot_heat=False):
+        heat_source=lambda x, t: [0], plot_heat=False,
+        method='CN'):
     """
     This function should return the solution to a parabolic
     partial differential equation
 
     USAGE: parabolic_pde_solver.pde_solve(L, T, u_I, mx, mt, f_kappa= lambda x: 1,
          logger=True, bcf=lambda t: [0,0], plot=False,
-         heat_source=lambda x, t: [0], plot_heat=False)
+         heat_source=lambda x, t: [0], plot_heat=False
+         method='CN')
 
     INPUT:
 
@@ -145,6 +287,10 @@ def pde_solve(L, T, u_I, mx, mt, f_kappa=lambda x: 1,
         mt: (array) discretiation points in time
 
         **optional** (default)
+        method: (CN) The method you wish to use to solve the pde (valid are CN, FE, BE)
+            CN: Crank-Nicholson (default)
+            FE: Forward Euler
+            BE: Backward Euler
         f_kappa: (lambda x: 1) specifies a variable diffusion coefficient over x
         logger: (True) specifies whether to log outputs to console
         bcf: (() => [0,0]) function that specifies the boundary conditions
@@ -164,22 +310,21 @@ def pde_solve(L, T, u_I, mx, mt, f_kappa=lambda x: 1,
 
     NOTE:
     """
-    # TODO where can one put forcing conditions?
     # set up the numerical environment variables
-    x = np.linspace(0, L, mx+1)      # mesh points in space # TODO 2d bar
+    x = np.linspace(0, L, mx+1)      # mesh points in space
     t = np.linspace(0, T, mt+1)      # mesh points in time
     deltax = x[1] - x[0]             # gridspacing in x
     deltat = t[1] - t[0]             # gridspacing in t
-    # create A_CN and B_CN matrices
-    A_CN=create_A_CN(x, f_kappa, deltat, deltax)
-    B_CN=create_B_CN(x, f_kappa, deltat, deltax)
-    #print diagonal matrices
-    if logger:
-         print("ACN to solve for each step: \n{}".format(A_CN))
-         print("BCN to solve for each step: \n{}".format(B_CN))
-    # print values we are using
-    if logger:
-        print("deltax=",deltax); print("deltat=",deltat);
+    # create necessary matrices
+    if method == 'CN':
+        A_CN=create_A_CN(x, f_kappa, deltat, deltax, logger=logger)
+        B_CN=create_B_CN(x, f_kappa, deltat, deltax, logger=logger)
+    elif method == 'FE':
+        [A_FE, lmbda_v]=create_A_FE(x, f_kappa, deltat, deltax, logger=logger)
+    elif method == 'BE':
+        A_BE = create_A_BE(x, f_kappa, deltat, deltax, logger=logger)
+    else:
+        raise ValueError("You have passed in an unrecognised method argument please ensure the method is one of (CN, BE, FE)")
     # set up the solution variables
     u_j = np.zeros(x.size)        # u at current time step
     u_jp1 = np.zeros(x.size)      # u at next time step
@@ -190,14 +335,19 @@ def pde_solve(L, T, u_I, mx, mt, f_kappa=lambda x: 1,
     H = []
     # Solve the PDE: loop over all time points
     for n in range(0, mt+1):
-        # dependent var to solve
-        b = B_CN.dot(u_j) # Forcing occurs here and should be added as a vector to b
         # calculate any added heat
         heat_j = heat_source(x, n)
-        # Backward Euler timestep solve matrix equation
-        u_jp1 = spsolve(A_CN, b) + heat_j
         # Boundary conditions
         [bc1, bc2] = bcf(n)
+        # solve for u_jp1
+        if method == 'CN':
+            u_jp1 = solve_CN(u_j, A_CN, B_CN, heat_j)
+        elif method == 'FE':
+            u_jp1 = solve_FE(u_j, A_FE, heat_j, lmbda_v, bc1, bc2)
+        elif method == 'BE':
+            u_jp1 = solve_BE(u_j, A_BE, heat_j)
+        else:
+            raise ValueError("You have passed in an unrecognised method argument please ensure the method is one of      (CN, BE, FE)")
         u_jp1[0] = bc1; u_jp1[mx] = bc2 #TODO dirchilet and neumann or mixed b cond will affect this part
         # Update u_j
         u_j = u_jp1
@@ -272,3 +422,12 @@ if __name__ == "__main__":
 
     [u_j, x, t] = pde_solve(L, T, u_I, mx, mt, plot=True, heat_source=heat_source, plot_heat=True)
     print('Final Solution with varying piecewise heat source inside the domain:\n{}'.format(u_j))
+
+    [u_j, x, t] = pde_solve(L, T, u_I, mx, mt, method='BE', plot=True, heat_source=heat_source)
+    print('Final Solution using Backward Euler:\n{}'.format(u_j))
+
+    # this does the heat source using the FE
+    [u_j, x, t] = pde_solve(L, T, u_I, mx, mt, method='FE', plot=True, heat_source=heat_source)
+    print('Final Solution using Forward Euler:\n{}'.format(u_j))
+
+
